b484638d39a8293ff282db94c6a5082bdab831fd
PawelMichnowicz <michnowicz013@gmail.com>
2022-06-26 00:56:22 +0200

    command wait_for_db created

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,11 +2,28 @@
 Django command to wait for the database to be available
 '''
 
+import time
+
+from psycopg2 import OperationalError as Psycopg2Error
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     ''' Django command to wait for database '''
 
-    def handle(self, *args, **options):
-        pass
+    def handle(self, *args, **options):  # handle - actual logic
+        self.stdout.write('Waiting for databese...')
+        db_up = False
+
+        while db_up is False:
+            try:
+                # BaseCommand.check - check entire django project,
+                # in this case check if databes is default, noqa
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Databaseis ready'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,30 @@
 '''
 Test custom Djano management commands
 '''
-from unittest.nock import patch
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
 
+
+# patch - this is command that we are going to be mocking 
+# and it adding a new argument for each calls - patched_check
+@patch('core.management.commands.wait_for_db.Command.check') # .check provided by BaseCommand (check method)
+class CommandTest(SimpleTestCase):
+
+    def test_wait_for_db_ready(self, patched_check):
+        patched_check.return_value = True # when check is call - return True
+
+        call_command('wait_for_db') # simulate running command
+
+        patched_check.assert_called_once_with(databases=['default']) # check if command is called once with correct db
+
+    @patch('time.sleep') # it replace sleep function with magic mock object, thanks that sleep is not execute during testing
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        patched_check.side_effect = [Psycopg2Error] * 3 + [OperationalError] * 2 + [True]  # simulate situation with 5 errors in row
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6) #check if command was called 6 times 
+        patched_check.assert_called_with(databases=['default'])
